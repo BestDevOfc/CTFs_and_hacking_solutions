fc93b19ce3acc7054767d78434a0a724fbb074c2
alimuhammadsecured <164618346+BestDevOfc@users.noreply.github.com>
2024-10-03 06:33:18 -0400

    Update shell_simple.s

--- Pwn/Shellcode/shell_simple.s
+++ Pwn/Shellcode/shell_simple.s
@@ -3,6 +3,11 @@
 # to parse the shellcode: objcopy --dump-section .text=shellcode_raw shellcodeelf 
 # when sending to vulnerable program: cat shellcode-raw; cat
 # reason we have the "cat" is that it'll immediately terminate once bin/sh is run we need to make it hang
+/*
+When debugging if you get a segfault you can do 
+x/i $rip to see what the next instruction was (where it crashed)
+
+*/
 .global _start
 _start:
 .intel_syntax noprefix
