092b5b8b2b95c2cf48dbaea42e3d3346b340e4b9
alimuhammadsecured <164618346+BestDevOfc@users.noreply.github.com>
2024-10-20 00:06:45 -0400

    Add files via upload

---
+++ WEB/JWT/NodeJS/index.js
@@ -0,0 +1,25 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+
+
+const PRIVATE_KEY = fs.readFileSync(path.join(__dirname, './keys/private'));
+const PUBLIC_KEY = fs.readFileSync(path.join(__dirname, './keys/public'));
+
+function sign(data){
+    return { data, signature : crypto.sign('RSA-SHA256', data, PRIVATE_KEY).toString('base64') };
+}
+
+function verify(signedUser){
+    const {data, signature } = signedUser;
+    return crypto.verify('RSA-SHA256', data, PUBLIC_KEY, Buffer.from(signature, 'base64'))
+}
+module.exports = {
+    sign,
+    verify
+};
+
+console.log(sign( 'charles' ));
+
+
+
