1b48c347ccb28594afeb63865d18c97903fa7ec7
alimuhammadsecured <164618346+BestDevOfc@users.noreply.github.com>
2024-11-08 20:42:08 -0500

    Create LSB_difference_in_images.py

---
+++ forensics_notes/Steg/Code/LSB_difference_in_images.py
@@ -0,0 +1,25 @@
+from PIL import Image
+
+im = Image.open('flowers.png', 'r')
+
+pix_val = list(im.getdata())
+
+im2 = Image.open('dylans_flowers.png', 'r')
+
+pix_val2 = list(im2.getdata())
+flag = ''
+for i in range(len(pix_val)):
+    one = pix_val[i]
+    two = pix_val2[i]
+    
+    # bcs in the "SUB" mode of stegsolve's "combine images" we see a small line of different colored pixels,
+    # she just stopped it at 35 from logically looking at the image.
+    if i <= 35:
+        print(i)
+        
+        # iterating every channel (if it's in RGBA mode)
+        for j in range(4):
+            print(bin(one[j]), bin(two[j]))
+            flag += bin(two[j])[-1]
+         
+print(flag)
